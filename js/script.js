afc8f11641336a89daaad2919717e132429bb41f
cariskao <hgfhhshgg@gmail.com>
2019-04-20 12:55:45 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+	//.parallax(xPosition, speedFactor, outerHeight) 設定選項:
+	//xPosition - 水平位置
+	//speedFactor - 滾動速度
+	$('.jumbotron').parallax("50%", 0.1);
+	$('.indexBG2').parallax("50%", 0.1);
+    $('.index4Icons').parallax("50%", 0.7);
+    $('.indexBG2').parallax("50%", 0.1);
+    $('.indexBG2').parallax("50%", 0.1);
+	$('.bg').parallax("50%", 0.4);
+});
